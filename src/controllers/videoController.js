68b4c62aec7d72da12a491bab75caa2ddf4d293d
denev <denev723@gmail.com>
2022-03-17 12:49:31 +0900

    pug 설치 및 기본설정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,11 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) =>
+    res.render("home", {
+        pageTitle: "Home"
+    });
 export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
+    return res.render("watch", {
+        pageTitle: "Watch"
+    });
 };
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
